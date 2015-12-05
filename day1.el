129df12f5f07de1e9e0a72dbd7e75e17dbdd95b0
Rolf Rander Næss <rolfn-github@pvv.org>
2015-12-05 16:15:52 +0100

    added solution for second puzzle of day 1

--- day1.el
+++ day1.el
@@ -3,13 +3,15 @@
 (defun count-parens (buffer)
   (with-current-buffer buffer
     (beginning-of-buffer)
-    (let ((level 0))
+    (let ((level 0)
+	  (basement 0))
       (while (< (point) (point-max))
 	(let ((c (char-after (point))))
 	  (cond ((= c ?() (incf level))
 		((= c ?)) (decf level))))
+	(if (and (= basement 0) (< level 0))
+	    (setf basement (point)))
 	(forward-char))
-      level)))
+      (cons level basement))))
 
 (count-parens "*advent-of-code-01*")
-
