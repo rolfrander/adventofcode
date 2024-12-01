70ffdf2b1837d1f57e1b6ae317ef8dba6d030ddf
rolfrander <rolfn-github@pvv.org>
2024-12-01 09:07:11 +0100

    day 01

---
+++ 2024/src/advent2024/day01.clj
@@ -0,0 +1,41 @@
+(ns advent2024.day01
+  (:require [clojure.string :as string]
+            [rolfrander.puzzle-lib :as puzzle-lib]
+            [rolfrander.puzzle-lib :as puzzle]))
+
+
+(def data (puzzle/get-data 2024 01))
+
+(def testdata "3   4
+4   3
+2   5
+1   3
+3   9
+3   3")
+
+(defn parse [input]
+  (->> (re-seq #"[0-9]+" input)
+       (map puzzle/str->long)
+       (partition 2)
+       (apply mapv vector)))
+
+(defn solve [in]
+  (let [[a b] (parse in)
+        a (sort a)
+        b (sort b)]
+    (->> (map #(Math/abs (- %1 %2)) a b)
+         (reduce +))))
+
+(defn solve-2 [in]
+  (let [[a b] (parse in)
+        b-freq (frequencies b)]
+    (->> (map #(* % (get b-freq % 0)) a)
+         (reduce +))))
+
+(solve testdata)
+(solve data)
+;;=> 1603498
+
+(solve-2 testdata)
+(solve-2 data)
+;;=> 25574739
