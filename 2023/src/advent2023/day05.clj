54fab62f0bc682d2082a5f6e42da150bac157c93
rolfrander <rolfn-github@pvv.org>
2024-12-26 20:04:21 +0100

    solved part 1, tried part 2

--- 2023/src/advent2023/day05.clj
+++ 2023/src/advent2023/day05.clj
@@ -44,7 +44,7 @@ humidity-to-location map:
      :offsets (reduce (fn [r e]
                     (conj r
                           (let [[dest src length] (map puzzle/str->long (str/split e #" "))]
-                            {:det dest
+                            {:dst dest
                              :src src
                              :len length})))
                   []
@@ -57,11 +57,75 @@ humidity-to-location map:
     {:seed (map puzzle/str->long (re-seq #"\d+" (first seed)))
      :maps maps}))
 
-(defn offset-map [m src]
+; dst < src
+;         dst      dst+len     src       src+len
+;      ----+----------+---------+-----------+------
+; d=     s      s          s      s-src+dst    s
+
+; dst > src
+;         src     src+len      dst       dst+len
+;      ----+----------+---------+-----------+------
+; d=     s  s-src+dst      s          s        s
+
+
+(defn offset-map-feil [src m]
   (cond (and (< src (:src m))
              (< src (:dst m))) src
-        
-        (< src (:src m)) (+ src (:len m))
 
-        (< src (:dst m))
-        ))
+        (and (>= src (+ (:src m) (:len m)))
+             (>= src (+ (:dst m) (:len m)))) src
+
+        (and (>= src (:src m))
+             (< src (+ (:src m) (:len m)))) (+ (- src (:src m)) (:dst m))
+
+        (and (>= src (+ (:src m) (:len m)))
+             (< src (+ (:dst m) (:len m)))) (- src (:len m))
+
+        :else
+        (+ src (:len m))))
+
+(defn offset-map [src m]
+  (if (and (>= src (:src m))
+           (< src (+ (:src m) (:len m))))
+    (+ (- src (:src m)) (:dst m))
+    nil))
+
+(let [d (parse testdata)
+      m (-> d :maps first :offsets)]
+  (map (juxt identity #(or (some (partial offset-map %) m) %))
+       (range 80 100))
+  )
+
+(offset-map 50 {:dst 50, :src 98, :len 2})
+
+(defn- map-seeds [maps s]
+  (loop [[{:keys [_from _to offsets]} & maps] maps
+         seed s]
+    (if (nil? offsets)
+      seed
+      (recur maps
+             (or (some (partial offset-map seed) offsets) seed)))))
+
+(defn solve-1 [in]
+  (let [d (parse in)]
+    (->> (map (partial map-seeds (:maps d))
+              (:seed d))
+         (apply min))))
+
+(defn solve-2 [in]
+  (let [d (parse in)
+        s (partition 2 (:seed d))]
+    (->> (mapcat #(map (partial map-seeds (:maps d))
+                       (range (first %) (+ (first %) (second %))))
+                 s)
+         (apply min))))
+
+
+(solve-1 testdata)
+;;=> 35
+(solve-1 (puzzle/get-data 2023 5))
+;;=> 484023871
+
+(solve-2 testdata)
+;;=> 46
+(solve-2 (puzzle/get-data 2023 5))
